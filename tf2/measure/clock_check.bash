990e7d8d3c550c2a57952de6ce56e8b51d7c8134
ogiwara <yushiogiwara@gmail.com>
2021-12-29 12:40:48 +0900

    add clock plot

---
+++ tf2/measure/clock_check.bash
@@ -0,0 +1,12 @@
+OUTPUT_DIR=/tmp/tf-clock/$(uuidgen)
+OUTPUT=$OUTPUT_DIR/data.dat
+OUTPUT_PLOT=$OUTPUT_DIR/plot.png
+
+mkdir -p $OUTPUT_DIR
+
+for T in $(seq 2 2 $(nproc)); do
+  ../cmake-build-release/devel/lib/tf2/clock_check --thread=$T --output=$OUTPUT
+done
+
+gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_PLOT';" clock.plt
+
--- tf2/measure/clock_check.cpp
+++ tf2/measure/clock_check.cpp
@@ -2,11 +2,13 @@
 #include <gflags/gflags.h>
 #include <thread>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 DEFINE_uint64(thread, std::thread::hardware_concurrency(), "thread num");
 DEFINE_uint64(iter, 10000, "iter");
+DEFINE_string(output, "/tmp/b.dat", "output file");
 
 int main(int argc, char* argv[]){
   gflags::SetUsageMessage("speed check");
@@ -37,7 +39,14 @@ int main(int argc, char* argv[]){
   for(auto &e: results){
     acc += e;
   }
-  cout << "delay: " << acc / FLAGS_thread << endl;
+  auto delay = acc / FLAGS_thread;
+  cout << "delay: " << delay << endl;
+
+  ofstream output{};
+  output.open(FLAGS_output.c_str(), std::ios_base::app);
+  output << FLAGS_thread << " ";
+  output << delay << endl;
+  output.close();
 
   return 0;
 }
