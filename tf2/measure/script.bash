e6b579d768f85e5ac3f86ee0db6015525c2962ca
ogiwara <yushiogiwara@gmail.com>
2022-01-04 08:33:59 +0900

    add latency plot

--- tf2/measure/script.bash
+++ tf2/measure/script.bash
@@ -9,6 +9,7 @@ OUTPUT=$OUTPUT_DIR/data.dat
 OUTPUT_PLOT=$OUTPUT_DIR/plot.png
 ONLY=0
 FREQUENCY=0
+OUT_OFFSET=11
 
 mkdir -p $OUTPUT_DIR
 
@@ -28,29 +29,6 @@ for T in $(seq 14 14 224); do
 ../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --iter=$I --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY --frequency=$FREQUENCY
 done
 
-#VAR=2
-#for J in 10 100 1000 10000; do
-#../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --iter=$I --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY
-#done
 
-#VAR=3
-#for I in 10 100 1000 10000 100000; do
-#../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --iter=$I --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY
-#done
 
-#VAR=4
-#for READ_RATIO in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-#../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --iter=$I --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY
-#done
-
-#VAR=5
-#for READ_LEN in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-#../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --iter=$I --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY
-#done
-
-#VAR=6
-#for WRITE_LEN in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-#../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --iter=$I --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY
-#done
-
-gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_PLOT'; var=$VAR" plot.plg
+gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_PLOT'; var=$VAR; out_offset=$OUT_OFFSET" plot.plg
