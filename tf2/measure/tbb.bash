184d91bf06a0b904cb3c9bc80877f193d3aae463
ogiwara <yushiogiwara@gmail.com>
2022-01-08 17:37:00 +0900

    add tbb check

---
+++ tf2/measure/tbb.bash
@@ -0,0 +1,12 @@
+OUTPUT_DIR=/tmp/tf-tbb/$(uuidgen)
+OUTPUT=$OUTPUT_DIR/data.dat
+OUTPUT_PLOT=$OUTPUT_DIR/plot.png
+
+mkdir -p $OUTPUT_DIR
+
+for T in $(seq 14 14 $(nproc)); do
+  ../cmake-build-release/devel/lib/tf2/tbb_check --thread=$T --output=$OUTPUT
+done
+
+gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_PLOT';" tbb.plt
+
---
+++ tf2/measure/tbb_check.cpp
@@ -0,0 +1,53 @@
+#include <chrono>
+#include <gflags/gflags.h>
+#include <thread>
+#include <iostream>
+#include <fstream>
+#include <tbb/concurrent_vector.h>
+
+using namespace std;
+
+DEFINE_uint64(thread, std::thread::hardware_concurrency(), "thread num");
+DEFINE_uint64(iter, 10000, "iter");
+DEFINE_string(output, "/tmp/c.dat", "output file");
+
+int main(int argc, char* argv[]){
+  gflags::SetUsageMessage("tbb check");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  cout << "thread: " << FLAGS_thread << endl;
+  cout << "iter: " << FLAGS_iter << endl;
+  cout << "Output: " << FLAGS_output << endl;
+
+  tbb::concurrent_vector<size_t> vec(FLAGS_thread);
+  bool wait=true;
+
+  vector<thread> threads{};
+  for(size_t t = 0; t < FLAGS_thread; t++){
+    threads.emplace_back([t, &wait, &vec](){
+      while (wait){;}
+
+      for(size_t i = 0; i < FLAGS_iter; i++){
+        vec[t]++;
+      }
+    });
+  }
+
+  auto start = chrono::high_resolution_clock::now();
+  wait = false;
+  for(auto &e: threads){
+    e.join();
+  }
+  auto finish = chrono::high_resolution_clock::now();
+  auto milli_sec = std::chrono::duration<double, std::milli>(finish - start).count();
+
+  cout << milli_sec << "ms" << endl;
+
+  ofstream output{};
+  output.open(FLAGS_output.c_str(), std::ios_base::app);
+  output << FLAGS_thread << " ";
+  output << std::chrono::duration<double, std::milli>(finish - start).count() << "ms" << endl;
+  output.close();
+
+  return 0;
+}
