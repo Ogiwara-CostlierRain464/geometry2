1e9e09f008047943631349535a8d90870cd7ee79
ogiwara <yushiogiwara@gmail.com>
2022-01-08 18:18:52 +0900

    update tbb_check

--- tf2/measure/tbb_check.cpp
+++ tf2/measure/tbb_check.cpp
@@ -4,34 +4,62 @@
 #include <iostream>
 #include <fstream>
 #include <tbb/concurrent_vector.h>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
 DEFINE_uint64(thread, std::thread::hardware_concurrency(), "thread num");
-DEFINE_uint64(iter, 10000, "iter");
 DEFINE_string(output, "/tmp/c.dat", "output file");
 
 double throughput(chrono::duration<double> time, size_t iter){
   return ((double) iter) * (1. / chrono::duration<double>(time).count());
 }
 
+template <typename T>
+struct CountAccum{
+  explicit CountAccum(size_t count): vec(count, T{}){}
+
+  void record(size_t t_id, const T &data){
+    vec[t_id] = data;
+  };
+
+  T sum(){
+    return std::accumulate(vec.begin(), vec.end(), T{});
+  }
+
+  std::vector<T> vec;
+};
+
+
 int main(int argc, char* argv[]){
   gflags::SetUsageMessage("tbb check");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   cout << "thread: " << FLAGS_thread << endl;
-  cout << "iter: " << FLAGS_iter << endl;
   cout << "Output: " << FLAGS_output << endl;
 
   tbb::concurrent_vector<size_t> vec(FLAGS_thread);
-  bool wait=true;
+  CountAccum<double> throughput_acc_thread(FLAGS_thread);
 
   vector<thread> threads{};
   for(size_t t = 0; t < FLAGS_thread; t++){
-    threads.emplace_back([t, &vec](){
-      for(size_t i = 0; i < FLAGS_iter; i++){
+    threads.emplace_back([t, &vec, &throughput_acc_thread](){
+
+      size_t iter_count = 0;
+      auto start_iter = chrono::steady_clock::now();
+      auto end_iter = start_iter;
+
+      for(;;){
         vec[t]++;
+
+        iter_count++;
+        end_iter = chrono::steady_clock::now();
+        if(end_iter - start_iter > 5s){
+          break;
+        }
       }
+      throughput_acc_thread.record(t, throughput(end_iter - start_iter, iter_count));
     });
   }
 
@@ -41,15 +69,13 @@ int main(int argc, char* argv[]){
   }
   auto finish = chrono::high_resolution_clock::now();
   auto sec = std::chrono::duration<double>(finish - start);
-  auto opes = FLAGS_thread * FLAGS_iter;
-  auto throughput_ = throughput(sec, opes);
+  auto throughput_ = throughput_acc_thread.sum();
 
   cout << throughput_ << "ope/sec" << endl;
 
   ofstream output{};
   output.open(FLAGS_output.c_str(), std::ios_base::app);
   output << FLAGS_thread << " ";
-  output << FLAGS_iter << " ";
   output << throughput_ << endl;
   output.close();
 
