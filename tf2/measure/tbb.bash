741c890ee106fc42fe20c620c7550dba7bc5015d
ogiwara <yushiogiwara@gmail.com>
2022-01-08 18:23:37 +0900

    fix iter, sec

--- tf2/measure/tbb.bash
+++ tf2/measure/tbb.bash
@@ -1,5 +1,3 @@
-I=10000
-
 OUTPUT_DIR=/tmp/tf-tbb/$(uuidgen)
 OUTPUT=$OUTPUT_DIR/data.dat
 OUTPUT_PLOT=$OUTPUT_DIR/plot.png
@@ -7,7 +5,7 @@ OUTPUT_PLOT=$OUTPUT_DIR/plot.png
 mkdir -p $OUTPUT_DIR
 
 for T in $(seq 14 14 224); do
-  ../cmake-build-release/devel/lib/tf2/tbb_check --thread=$T --iter=$I --output=$OUTPUT
+  ../cmake-build-release/devel/lib/tf2/tbb_check --thread=$T --output=$OUTPUT
 done
 
 gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_PLOT';" tbb.plt
