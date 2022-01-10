a3b8637909df63411caf3cb870024f842453297c
ogiwara <yushiogiwara@gmail.com>
2022-01-10 09:30:23 +0900

    update script.bash

--- tf2/measure/script.bash
+++ tf2/measure/script.bash
@@ -4,6 +4,7 @@ READ_RATIO=1
 READ_LEN=16
 WRITE_LEN=16
 FREQUENCY=0
+LOOP_SEC=60
 
 OUTPUT_DIR=/tmp/tf/$(uuidgen)
 OUTPUT=$OUTPUT_DIR/data.dat
@@ -28,7 +29,7 @@ for T in $(seq 14 14 224); do
 #for WRITE_LEN in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
 #VAR=6
 #for FREQUENCY in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY --frequency=$FREQUENCY
+../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY --frequency=$FREQUENCY --loop_sec=$LOOP_SEC
 done
 
 gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_PLOT'; var=$VAR; out_offset=$OUT_OFFSET" plot.plg
