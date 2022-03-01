d6c161685faee0a8177585db0a2fd7dcfa940ec4
ogiwara <yushiogiwara@gmail.com>
2022-03-01 13:47:38 +0900

    update car

--- tf2/measure/car.bash
+++ tf2/measure/car.bash
@@ -13,7 +13,7 @@ OUTPUT_PLOT=$OUTPUT_DIR/plot.png
 mkdir -p $OUTPUT_DIR
 
 for F in 10 100 1000; do
-../cmake-build-release/devel/lib/tf2/car_check --vehicle=$V --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --frequency=$F --loop_sec=$LOOP_SEC --read_thread=$READ_T --write_thread=$WRITE_T
+../cmake-build-release/devel/lib/tf2/car_check --vehicle=$V --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --frequency=$F --loop_sec=$LOOP_SEC --read_thread=$READ_T --write_thread=$WRITE_T --only=0
 done
 
 gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_PLOT';" car.plt
