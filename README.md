8469afb5918b35d109631e245baa12d9d9bb98f4
ogiwara <yushiogiwara@gmail.com>
2022-04-06 11:28:29 +0900

    add MAKE_READ_STAT flag to script.bash

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ you have to reinstall tf2, tf2_py, tf2_ros, rviz, and tf
 be sure to recompile dirty include files with -MMD -MP flag!!!
 
 
+# Reproduction step
+
+```bash
+#1 clone with intel tbb library.
+git clone --recursive git@github.com:Ogiwara-CostlierRain464/geometry2.git
+#2 mkdir -p
+#3 source build.bash
+#4 source script.bash
+
+
+# contents of script.bash:
+
+```
+
--- tf2/measure/script.bash
+++ tf2/measure/script.bash
@@ -6,6 +6,7 @@ WRITE_LEN=16
 FREQUENCY=0
 LOOP_SEC=60
 OPPOSITE_WRITE=true
+MAKE_READ_STAT=false
 
 OUTPUT_DIR=/tmp/tf/$(uuidgen)
 OUTPUT=$OUTPUT_DIR/data.dat
@@ -20,17 +21,7 @@ mkdir -p $OUTPUT_DIR
 # Thread, Joint, Iter, Read_Ratio, Read_Len, Write_len
 VAR=1
 for T in $(seq 28 28 224); do
-#VAR=2
-#for J in 10 100 1000 10000; do
-#VAR=3
-#for READ_RATIO in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-#VAR=4
-#for READ_LEN in 1000 9990 do
-#VAR=5
-#for WRITE_LEN in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-#VAR=6
-#for FREQUENCY in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
-../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY --frequency=$FREQUENCY --loop_sec=$LOOP_SEC --opposite_write_direction=$OPPOSITE_WRITE
+../cmake-build-release/devel/lib/tf2/speed_check --thread=$T --joint=$J --read_ratio=$READ_RATIO --read_len=$READ_LEN --write_len=$WRITE_LEN --output=$OUTPUT --only=$ONLY --frequency=$FREQUENCY --loop_sec=$LOOP_SEC --opposite_write_direction=$OPPOSITE_WRITE --make_read_stat=$MAKE_READ_STAT
 done
 
 gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_PLOT'; var=$VAR; out_offset=$OUT_OFFSET; only=$ONLY" plot.plg
