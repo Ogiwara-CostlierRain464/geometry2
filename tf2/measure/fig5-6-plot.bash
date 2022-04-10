e3123eb39fb61a474db780b7bb8f274bf6b88a90
ogiwara <yushiogiwara@gmail.com>
2022-04-10 16:43:10 +0900

    fix only flag in plot bash

--- tf2/measure/fig5-6-plot.bash
+++ tf2/measure/fig5-6-plot.bash
@@ -1,2 +1,2 @@
-gnuplot -e "data='/tmp/fig5-6.dat'; filename='/tmp/fig5.pdf'; var=1; out_offset=7;" plot.plg
-gnuplot -e "data='/tmp/fig5-6.dat'; filename='/tmp/fig6.pdf'; var=1; out_offset=11;" plot.plg
+gnuplot -e "data='/tmp/fig5-6.dat'; filename='/tmp/fig5.pdf'; var=1; out_offset=7; only=0" plot.plg
+gnuplot -e "data='/tmp/fig5-6.dat'; filename='/tmp/fig6.pdf'; var=1; out_offset=11; only=0" plot.plg
--- tf2/measure/fig7,9-plot.bash
+++ tf2/measure/fig7,9-plot.bash
@@ -1,3 +1,3 @@
-gnuplot -e "data='/tmp/fig7,9.dat'; filename='/tmp/fig7.pdf'; var=1; out_offset=7;" plot.plg
+gnuplot -e "data='/tmp/fig7,9.dat'; filename='/tmp/fig7.pdf'; var=1; out_offset=7; only=0" plot.plg
 gnuplot -e "data='/tmp/fig7,9.dat'; filename='/tmp/fig9.pdf'; var=1; out_offset=11; only=3" plot.plg
 
