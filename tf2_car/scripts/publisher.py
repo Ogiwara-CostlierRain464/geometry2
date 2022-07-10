5f5fde09e6b21698d428a93f3a885e3cd01b694b
ogiwara <yushiogiwara@gmail.com>
2022-07-10 13:37:09 +0900

    update publisher.py

--- tf2_car/scripts/publisher.py
+++ tf2_car/scripts/publisher.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
+import tf
 import tf2_ros
+import tf2_msgs
+import tf2_geometry_msgs
 import rospy
 
 if __name__ == "__main__":
-    rospy.init_node("publisher")
-    # build thread and publish?
-    rospy.spin()
+    rospy.init_node("publisher", anonymous=True)
+    # publishes every 150Hz.
+    br = tf.TransformBroadcaster()
+    r = rospy.Rate(150)
+    while not rospy.is_shutdown():
+        br.sendTransform((0, 0, 0), (0, 0, 0, 1), rospy.Time.now(), "a", "b")
+        r.sleep()
