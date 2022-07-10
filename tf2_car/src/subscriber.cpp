d273dd8b84e9bd44a85a49278c06f0e2a854d31a
ogiwara <yushiogiwara@gmail.com>
2022-07-10 14:46:22 +0900

    add subscriber.cpp

---
+++ tf2_car/src/subscriber.cpp
@@ -0,0 +1,12 @@
+
+#include <tf2_ros/buffer_server.h>
+#include <tf2_ros/transform_listener.h>
+#include <ros/ros.h>
+
+int main(int argc, char** argv){
+  // continuously read from action_server
+  // ref
+  // many nodes
+
+  return 0;
+}
