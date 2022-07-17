7b732e44f2c525dd3a28a74148793268faf7d7ea
ogiwara <yushiogiwara@gmail.com>
2022-07-17 09:06:09 +0900

    update data generate script

--- tf2/measure/fig5-6-data.bash
+++ tf2/measure/fig5-6-data.bash
@@ -4,5 +4,5 @@
 ONLY=1111
 
 for T in $(seq 28 28 224); do
-/root/ros_ws/devel/lib/tf2/many_snakes --thread=$T --snake_num=10000 --snake_joints=100 --read_ratio=1 --read_len=8 --write_len=8 --output="/tmp/fig5-6.dat" --only=$ONLY --frequency=0 --loop_sec=60 --opposite_write_direction=true --make_read_stat=false
+/root/ros_ws/devel/lib/tf2/speed_check --thread=$T --joint=1000000 --read_ratio=1 --read_len=16 --write_len=16 --output="/tmp/fig5-6.dat" --only=$ONLY --frequency=0 --loop_sec=60 --opposite_write_direction=true --make_read_stat=false
 done
