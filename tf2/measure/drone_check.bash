95ac119e5cbbb188e41bd3ad0a0f2dbbe32c9d8f
ogiwara <yushiogiwara@gmail.com>
2022-11-01 15:22:40 +0900

    fix output file path

--- tf2/measure/drone_check.bash
+++ tf2/measure/drone_check.bash
@@ -3,7 +3,7 @@ OUTPUT=$OUTPUT_DIR/data.dat
 mkdir -p $OUTPUT_DIR
 
 for T in $(seq 28 28 224); do
-/root/ros_ws/devel/lib/tf2/drone_check --thread=$T
+/root/ros_ws/devel/lib/tf2/drone_check --thread=$T --output=$OUTPUT
 done
 
 gnuplot -e "data='$OUTPUT'; filename='$OUTPUT_DIR/read-latency.pdf'; out_offset=3" drone.plt
