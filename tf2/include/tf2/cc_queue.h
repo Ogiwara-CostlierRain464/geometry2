ace350a4c0a989c018a3efa716f310340d3587dc
ogiwara <yushiogiwara@gmail.com>
2022-12-11 14:42:09 +0900

    try new CC queue

--- tf2/include/tf2/cc_queue.h
+++ tf2/include/tf2/cc_queue.h
@@ -5,11 +5,12 @@
 #include <atomic>
 #include "transform_storage.h"
 
-#define CC_ARR_SIZE 5
+#define CC_ARR_SIZE 20
 
 class CCNode{
 public:
   std::atomic_int cur{-1};
+  bool filled = false;
   std::array<tf2::TransformStorage, CC_ARR_SIZE> arr{};
   CCNode *next{nullptr};
 
@@ -69,37 +70,42 @@ public:
 
 class CCQueue{
 private:
-  CCNode firstNode{};
-  CCNode *current{&firstNode};
-  int fulledNodeNum = 0;
+  std::atomic_int cur{-1};
+  bool filled = false;
+  std::array<tf2::TransformStorage, CC_ARR_SIZE> arr{};
+
 public:
   void insert(const tf2::TransformStorage &e){
-//    bool node_changed = false;
-//    current->copyAndInsert(e, node_changed);
-//    if(node_changed){
-//      fulledNodeNum++;
-//      current = current->next;
-//    }
-    if(firstNode.cur == -1){
-      firstNode.arr[0] = e;
-      firstNode.cur = 0;
-    }else{
-      if(firstNode.cur == CC_ARR_SIZE - 1){
-        // max
-        firstNode.arr[CC_ARR_SIZE-1] = e;
+    if(filled){
+      if(cur == CC_ARR_SIZE - 1){
+        arr[0] = e;
+        cur = 0;
       }else{
-        firstNode.arr[firstNode.cur+1] = e;
-        firstNode.cur++;
+        arr[cur+1] = e;
+        cur++;
+      }
+    }else{
+      if(cur == CC_ARR_SIZE - 1){
+        filled = true;
+        arr[0] = e;
+        cur = 0;
+      }else{
+        arr[cur+1] = e;
+        cur++;
       }
     }
   }
 
   bool empty() const{
-    return firstNode.size() == 0;
+    return cur == -1;
   }
 
   int size() const{
-    return CC_ARR_SIZE * fulledNodeNum + current->size();
+    if(filled){
+      return CC_ARR_SIZE;
+    }else{
+      return cur+1;
+    }
   }
 
   tf2::TransformStorage& front(){
@@ -107,19 +113,54 @@ public:
   }
 
   tf2::TransformStorage& latest(){
-    return current->latest();
+    return arr[cur];
   }
 
   tf2::TransformStorage& first(){
-    // First element is always the oldest data.
-    return firstNode.arr[0];
+    if(!filled){
+      return arr[0];
+    }else{
+      // linear search
+      ros::Time tmp = ros::TIME_MAX;
+      int id;
+      for(int i = 0; i < CC_ARR_SIZE; i++){
+        if(arr[i].stamp_ < tmp){
+          tmp = arr[i].stamp_;
+          id  = i;
+        }
+      }
+
+      return arr[id];
+    }
   }
 
   void findTwoClose(const ros::Time &target,
-                    tf2::TransformStorage* &one,
-                    tf2::TransformStorage* &two){
-    one = &firstNode.arr[0];
-    two = &firstNode.arr[0];
+                    tf2::TransformStorage &one,
+                    tf2::TransformStorage &two){
+    // linear search
+    // bound from up and down
+    tf2::TransformStorage *one_tmp, *two_tmp;
+    ros::Time one_t = ros::TIME_MIN, two_t = ros::TIME_MAX;
+
+    int seek_till = filled ? CC_ARR_SIZE : cur.load();
+
+    for(int i = 0; i <= seek_till; i++){
+      auto &time = arr[i].stamp_;
+      if(target < time){
+        if(time < two_t){
+          two_tmp = &arr[i];
+          two_t = time;
+        }
+      }else{ // target >= time
+        if(time > one_t){
+          one_tmp = &arr[i];
+          one_t = time;
+        }
+      }
+    }
+
+    one = *one_tmp;
+    two = *two_tmp;
   }
 
   void clear(){
