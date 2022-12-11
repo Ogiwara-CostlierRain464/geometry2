7aaba6c37bafa7a12ea3bb2e0017702b5ce5e17f
ogiwara <yushiogiwara@gmail.com>
2022-12-11 16:52:02 +0900

    try to use copy moving CC

--- tf2/include/tf2/cc_queue.h
+++ tf2/include/tf2/cc_queue.h
@@ -71,29 +71,33 @@ public:
 class CCQueue{
 private:
   std::atomic_int cur{-1};
-  bool filled = false;
   std::array<tf2::TransformStorage, CC_ARR_SIZE> arr{};
 
 public:
   void insert(const tf2::TransformStorage &e){
-    if(filled){
-      if(cur == CC_ARR_SIZE - 1){
-        arr[0] = e;
-        cur = 0;
-      }else{
-        arr[cur+1] = e;
-        cur++;
-      }
-    }else{
-      if(cur == CC_ARR_SIZE - 1){
-        filled = true;
-        arr[0] = e;
-        cur = 0;
-      }else{
-        arr[cur+1] = e;
-        cur++;
+    // copy and insert.
+    int insert_point = cur+1;
+    for(int i  = 0; i < cur; i++){
+      if(e.stamp_ < arr[i].stamp_){
+        insert_point = i;
+        break;
       }
     }
+    if(insert_point == CC_ARR_SIZE){
+      printf("FAIL\n");
+    }else{
+      int i = cur;
+      for(;;){
+        arr[i+1] = arr[i];
+        i--;
+        if(i < insert_point){
+          break;
+        }
+      }
+    }
+
+    arr[insert_point] = e;
+    cur++;
   }
 
   bool empty() const{
@@ -101,11 +105,7 @@ public:
   }
 
   int size() const{
-    if(filled){
-      return CC_ARR_SIZE;
-    }else{
-      return cur+1;
-    }
+    return cur+1;
   }
 
   tf2::TransformStorage& front(){
@@ -117,50 +117,23 @@ public:
   }
 
   tf2::TransformStorage& first(){
-    if(!filled){
-      return arr[0];
-    }else{
-      // linear search
-      ros::Time tmp = ros::TIME_MAX;
-      int id;
-      for(int i = 0; i < CC_ARR_SIZE; i++){
-        if(arr[i].stamp_ < tmp){
-          tmp = arr[i].stamp_;
-          id  = i;
-        }
-      }
-
-      return arr[id];
-    }
+    return arr[0];
   }
 
   void findTwoClose(const ros::Time &target,
                     tf2::TransformStorage* &one,
                     tf2::TransformStorage* &two){
-    // linear search
-    // bound from up and down
-    tf2::TransformStorage *one_tmp, *two_tmp;
-    ros::Time one_t = ros::TIME_MIN, two_t = ros::TIME_MAX;
+    tf2::TransformStorage storage_target_time;
+    storage_target_time.stamp_ = target;
 
-    int seek_till = filled ? CC_ARR_SIZE : cur.load();
+    auto it = std::lower_bound(
+      arr.begin(),
+      arr.begin() + cur,
+      storage_target_time,
+      std::greater<tf2::TransformStorage>());
 
-    for(int i = 0; i <= seek_till; i++){
-      auto &time = arr[i].stamp_;
-      if(target < time){
-        if(time < two_t){
-          two_tmp = &arr[i];
-          two_t = time;
-        }
-      }else{ // target >= time
-        if(time > one_t){
-          one_tmp = &arr[i];
-          one_t = time;
-        }
-      }
-    }
-
-    one = one_tmp;
-    two = two_tmp;
+    one = &*(it); //Older
+    two = &*(--it); //Newer
   }
 
   void clear(){
