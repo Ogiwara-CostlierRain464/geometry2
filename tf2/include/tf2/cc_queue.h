0195d85160741c91defa7e0fb27c22e80f017525
ogiwara <yushiogiwara@gmail.com>
2022-12-11 21:10:35 +0900

    update cc queue

--- tf2/include/tf2/cc_queue.h
+++ tf2/include/tf2/cc_queue.h
@@ -7,97 +7,22 @@
 
 #define CC_ARR_SIZE 20
 
-class CCNode{
-public:
-  std::atomic_int cur{-1};
-  bool filled = false;
-  std::array<tf2::TransformStorage, CC_ARR_SIZE> arr{};
-  CCNode *next{nullptr};
-
-  void copyAndInsert(const tf2::TransformStorage &e, bool &node_changed){
-    bool insert_middle = true;
-    int insert_point = cur+1;
-    for(int i = 0; i <= cur; i++){
-      if(e.stamp_ < arr[i].stamp_){
-        insert_point = i;
-        break;
-      }
-    }
-
-    if(insert_point == CC_ARR_SIZE){
-      auto tmp = new CCNode{};
-      tmp->arr[0] = e;
-      tmp->cur = 0;
-      next = tmp;
-      node_changed = true;
-    }else{
-      assert(insert_middle);
-      int i = cur;
-      for(;;){
-        if(i+1 == CC_ARR_SIZE){
-          // need to move to next node!
-          auto tmp = new CCNode{};
-          tmp->arr[0] = arr[i];
-          tmp->cur = 0;
-          next = tmp;
-          node_changed = true;
-        }else{
-          arr[i+1] = arr[i];
-        }
-        i--;
-        if(i < insert_point){
-          break;
-        }
-      }
-
-      arr[insert_point] = e;
-      cur++;
-    }
-  }
-
-  bool isFull() const{
-    return size() == CC_ARR_SIZE;
-  }
-
-  int size() const{
-    return cur+1;
-  }
-
-  tf2::TransformStorage& latest(){
-    return arr[cur];
-  }
-};
-
 class CCQueue{
 private:
   std::atomic_int cur{-1};
+  bool filled = false;
   std::array<tf2::TransformStorage, CC_ARR_SIZE> arr{};
 
 public:
   void insert(const tf2::TransformStorage &e){
-    // copy and insert.
-    int insert_point = cur+1;
-    for(int i  = 0; i < cur; i++){
-      if(e.stamp_ < arr[i].stamp_){
-        insert_point = i;
-        break;
-      }
-    }
-    if(insert_point == CC_ARR_SIZE){
-      printf("FAIL\n");
+    if(cur == CC_ARR_SIZE - 1){
+      filled = true;
+      arr[1] = e;
+      cur = 1;
     }else{
-      int i = cur;
-      for(;;){
-        arr[i+1] = arr[i];
-        i--;
-        if(i < insert_point){
-          break;
-        }
-      }
+      arr[cur+1] = e;
+      cur++;
     }
-
-    arr[insert_point] = e;
-    cur++;
   }
 
   bool empty() const{
@@ -105,7 +30,11 @@ public:
   }
 
   int size() const{
-    return cur+1;
+    if(filled){
+      return CC_ARR_SIZE;
+    }else{
+      return cur+1;
+    }
   }
 
   tf2::TransformStorage& front(){
@@ -123,17 +52,30 @@ public:
   void findTwoClose(const ros::Time &target,
                     tf2::TransformStorage* &one,
                     tf2::TransformStorage* &two){
-    tf2::TransformStorage storage_target_time;
-    storage_target_time.stamp_ = target;
+    // linear search
+    // bound from up and down
+    tf2::TransformStorage *one_tmp, *two_tmp;
+    ros::Time one_t = ros::TIME_MIN, two_t = ros::TIME_MAX;
 
-    auto it = std::lower_bound(
-      arr.begin(),
-      arr.begin() + cur,
-      storage_target_time,
-      std::greater<tf2::TransformStorage>());
+    int seek_till = filled ? CC_ARR_SIZE : cur.load();
 
-    one = &*(it); //Older
-    two = &*(++it); //Newer
+    for(int i = 0; i <= seek_till; i++){
+      auto &time = arr[i].stamp_;
+      if(target < time){
+        if(time < two_t){
+          two_tmp = &arr[i];
+          two_t = time;
+        }
+      }else{ // target >= time
+        if(time > one_t){
+          one_tmp = &arr[i];
+          one_t = time;
+        }
+      }
+    }
+
+    one = one_tmp;
+    two = two_tmp;
   }
 
   void clear(){
